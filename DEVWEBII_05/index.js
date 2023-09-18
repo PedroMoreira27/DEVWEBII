737d0a67660c09960a53e548040c1f6de3d4baa8
PEDRO GONCALVES MOREIRA <1091392313013@fatec-srv.local>
2023-09-18 20:28:36 -0300

    18/09/23

---
+++ DEVWEBII_05/index.js
@@ -0,0 +1,42 @@
+const {createApp} = Vue;
+createApp({
+    data(){
+        return {
+            display: 0,
+            numeroAtual: null,
+            numeroAnterior: null,
+            operador: null
+        }
+    },
+    methods: {
+        lidarBotao(botao){
+            // console.log(botao)
+            switch(botao){
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    this.lidarOperador(botao);
+                    break;
+                case "=":
+                    this.lidarIgual(botao);
+                    break;
+                case ".":
+                    this.lidarDecimal(botao);
+                    break;
+                default:
+                    this.lidarNumeros(botao);
+                    break;                           
+            }
+        },
+
+        lidarNumeros(numero){
+            if(this.display == 0){
+                console.log(this.display)
+                this.display = "";
+            }else{
+                this.display += numero.toString();
+            }
+        }
+    }
+}).mount('#app')
